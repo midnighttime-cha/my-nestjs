117a203ebd32886522c9c903f5c7cb2bd975ce98
Chawalit Sengsang <chawalit1987@gmail.com>
2022-09-08 14:35:22 +0700

    config main.js

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication, ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+const bodyParser = require('body-parser');
+const port = process.env.API_PORT;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  //ตั้งค่าให้ NestJS ใช้ Express Platform
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter());
+
+  // ตั้งค่า CORS
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(port);
 }
 bootstrap();
